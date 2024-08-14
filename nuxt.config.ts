146105964e26a9cb6e9719abbc19278cb162f42e
Hans Fredrik Maast <hfmaast@gmail.com>
2024-08-14 09:54:26 +0200

    refactor: move eslint config to eslint.config.mjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/devtools', '@nuxtjs/tailwindcss'],
-  eslint: {
-    config: {
-      stylistic: true, // allows formatting as well as linting
-    },
-  },
 })
